ae3f546e12ea824a75089faae6777fa347fc9993
王慧芳 <huifangwang10@creditease.cn>
2021-11-03 16:16:32 +0800

    弹幕

--- src/views/css/TitleClamp.vue
+++ src/views/css/TitleClamp.vue
@@ -14,6 +14,18 @@
         <div class="desc">有人说她有能力成就今天的成就完全是靠刘强东。事实上，事实并非如此，她能够取得今天的成就，除了刘强东的关系牢固以外，也靠着她足够的聪明，足够的努力。</div>
       </div>
     </div>
+    <!-- 实现效果：弹幕 -->
+    <div class="chat_container">
+      <ul class="chat_lines">
+        <li v-for="item in 15" :key="item">第{{item}}条</li>
+      </ul>
+      <ul class="chat_lines">
+        <li v-for="item in 15" :key="item">第{{item + 15}}条</li>
+      </ul>
+      <ul class="chat_lines">
+        <li v-for="item in 15" :key="item">第{{item + 30}}条</li>
+      </ul>
+    </div>
   </div>
 </template>
 <style lang="less" scoped>
@@ -54,11 +66,51 @@
     }
     &::after{
       content: '';
-      width: 999vh;
-      height: 999vh;
+      max-width: 999vh;
+      // height: 999vh;
+      height: 5rem;
       background-color: #fff;
       position: absolute;
       box-shadow: -2em 2em #fff;
+    }
+  }
+}
+.chat{
+  &_container{
+    width: 100%;
+    height: 300px;
+    margin: 100px auto 0;
+    border: 3px dotted gray;
+    box-sizing: border-box;
+    overflow: hidden;
+  }
+  &_lines{
+    margin: 20px 0;
+    display: flex;
+    margin-left: 100%;
+    animation: move 20s linear infinite;
+    @keyframes move {
+      0% {
+        transform: translateX(0);
+      }
+      100% {
+        transform: translateX(-2250px);
+      }
+    }
+    &:first-child{
+      padding-top: 50px;
+    }
+    li{
+      display: flex;
+      align-items: center;
+      flex-shrink: 0;
+      width: 100px;
+      height: 50px;
+      padding-left: 20px;
+      border-radius: 3px;
+      text-align: center;
+      background-color: rgb(127, 81, 236);
+      margin-right: 30px;
     }
   }
 }
